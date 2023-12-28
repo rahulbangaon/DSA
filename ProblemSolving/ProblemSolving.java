f544ff56195671192b005192d903b56db7e2e644
Rahul Dhaka Bangaon <rahulbangaon@yahoo.com>
2023-12-28 17:24:17 +0530

    Revert "Problem Solving"
    This reverts commit 65e3c661d499aabde24cb9216a2b350210eb3f2a.

--- ProblemSolving/ProblemSolving.java
+++ ProblemSolving/ProblemSolving.java
@@ -1,37 +1,24 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class ProblemSolving {
+    public class ProblemSolving {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ProblemSolving obj = new ProblemSolving();
-        System.out.println("Enter Number to Count Factors:");
-        int num = sc.nextInt();
-        int ans = obj.factorCounter(num);
-        System.out.println("Number of Factors of given no. is - " + ans);
+    Scanner sc = new Scanner(System.in);
+    ProblemSolving obj = new ProblemSolving();
+    System.out.println("Enter Number to Count Factors:");
+    int num = sc.nextInt();
+    int ans = obj.factorCounter(num);
+    System.out.println("Number of Factors of given no. is - " + ans);
 
-        sc.close();
+
+    sc.close();
     }
 
     int factorCounter(int num) {
-        int count = 0;
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0)
-                count++;
-        }
-        return count;
+    int count = 0;
+    for (int i = 1; i <= num; i++) {
+        if(num%i==0)
+            count++;
+    } 
+    return count;
     }
-
-    int factorCounterOptimized(int num) {
-        int count = 0;
-        for (int i = 0; i * i < num; i++) {
-            if (num % i == 0) {
-                if (i == num / i) {
-                    count++;
-                } else {
-                    count += 2;
-                }
-            }
-        }
-        return count;
     }
-}
